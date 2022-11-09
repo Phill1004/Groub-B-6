24d71a3795acc64b210a3c259d48db7fa1f465b9
Phillip k Kim <94643432+Phill1004@users.noreply.github.com>
2022-11-09 16:20:45 -0600

    Update chessPieces.java

--- chessPieces.java
+++ chessPieces.java
@@ -25,17 +25,49 @@ public class chessPieces {
         
         return name; 
     }
-    void knight(int color, int positionX, int positionY){
-
+    String knight(int pieceColor, int positionX, int positionY){
+        color = pieceColor;
+        x = positionX;
+        y = positionY;
+        if (pieceColor == 0)
+            name = "WKnight";
+        else if (pieceColor == 1)
+            name = "BKnight";
+        
+        return name; 
     }
-    void bishop(int color, int positionX, int positionY){
-
+    String bishop(int pieceColor, int positionX, int positionY){
+        color = pieceColor;
+        x = positionX;
+        y = positionY;
+        if (pieceColor == 0)
+            name = "WBishop";
+        else if (pieceColor == 1)
+            name = "BBishop";
+        
+        return name; 
     }
-    void queen(int color, int positionX, int positionY){
-
+    String queen(int pieceColor, int positionX, int positionY){
+        color = pieceColor;
+        x = positionX;
+        y = positionY;
+        if (pieceColor == 0)
+            name = "WQueen";
+        else if (pieceColor == 1)
+            name = "BQueen";
+        
+        return name; 
     }
-    void king(int color, int positionX, int positionY){
-
+    String king(int pieceColor, int positionX, int positionY){
+        color = pieceColor;
+        x = positionX;
+        y = positionY;
+        if (pieceColor == 0)
+            name = "WKing";
+        else if (pieceColor == 1)
+            name = "BKing";
+        
+        return name; 
     }
 
 }
