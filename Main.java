cbd087800a5706d7572c6e2d80475b41c4742abe
PrestonZ82 <70246441+PrestonZ82@users.noreply.github.com>
2022-11-10 18:12:47 -0600

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,12 +1,43 @@
 public class Main {
     public static void main(String[] args) {
-        gameBoard theGameBoard = new gameBoard();
+        chessPieces pieces = new chessPieces();
+        String[][] board = new String[8][8];
+        //sets black pieces
+        board[0][0] = pieces.rook(1, 0, 0);
+        board[0][1] = pieces.knight(1, 0, 1);
+        board[0][2] = pieces.bishop(1, 0, 2);
+        board[0][3] = pieces.queen(1, 0, 3);
+        board[0][4] = pieces.king(1, 0, 4);
+        board[0][5] = pieces.bishop(1, 0, 5);
+        board[0][6] = pieces.knight(1, 0, 6);
+        board[0][7] = pieces.rook(1, 0, 7);
+        for(int i = 0; i < 8; i++){
+            board[1][i] = pieces.pawn(1, 1, i);
+        }
+        //sets white pieces
+        board[7][0] = pieces.rook(0, 7, 0);
+        board[7][1] = pieces.knight(0, 7, 1);
+        board[7][2] = pieces.bishop(0, 7, 2);
+        board[7][3] = pieces.queen(0, 7, 3);
+        board[7][4] = pieces.king(0, 7, 4);
+        board[7][5] = pieces.bishop(0, 7, 5);
+        board[7][6] = pieces.knight(0, 7, 6);
+        board[7][7] = pieces.rook(0, 7, 7);
+        for(int i = 0; i < 8; i++){
+            board[6][i] = pieces.pawn(0, 6, i);
+        }
+        //sets empty spaces
+        for(int row = 2; row < 6; row++){
+            for(int col = 0; col < 8; col++){
+                board[row][col] = " ";
+            }
+        }
 
-        theGameBoard.printBoard();
-        //testing the integer.parseint to try to get the first number out of the 2 numbers in temp
-        int temp = 03;
-        System.out.println(Integer.parseInt(Integer.toString(temp).substring(0)));
-        //System.out.println(Integer.parseInt(Integer.toString(temp).substring(1)));
-
+        for(int row = 0; row < 8; row++){
+            System.out.println();
+            for(int col = 0; col < 8; col++){
+                System.out.print(board[row][col]);
+            }
+        }
     }
 }
