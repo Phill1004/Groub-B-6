211ecf58008d8922ca7a1c3556c81be1685a1e5d
NickJordan7 <117865358+NickJordan7@users.noreply.github.com>
2022-12-02 16:46:29 -0600

    Update clock.java

--- clock.java
+++ clock.java
@@ -3,22 +3,48 @@ import java.util.TimerTask;
 
 public class clock {
     Timer t;
-    int minutes = 0; // 15 minute games
-    boolean timerEnded = false;
+    int minutes = 15;
+    boolean turn = false;
     String name = "";
+    int elapsedTime = 0;
 
     // create the new timer and schedule the timer to run for 15 minutes
-    public void countDownTimer() {
-        t = new Timer();
-        t.schedule(new rt(), minutes * 60 * 1000); // convert to milliseconds
+    public void runTimer(int elapsedTime) {
+        if(elapsedTime == (minutes * 60))
+            System.out.println(name +"'s time has run out!");
+
+        // if it is the player's turn then run a 1 second timer
+        if(turn = true)
+        {
+            t = new Timer();
+            t.schedule(new rt(), 1000); // convert to milliseconds
+        }
     }
 
     // run timer and print when finished
     class rt extends TimerTask {
         public void run() {
-            System.out.println(name + "'s timer has finished.");
-            timerEnded = true;
+            elapsedTime++;
+            //timeLeft(elapsedTime);
             t.cancel(); // end timer
+            runTimer(elapsedTime);
         }
     }
+
+    public void timeLeft(int time)
+    {
+        int min;
+        int sec;
+
+        // calculate time in minutes and seconds
+        min = minutes - (time / 60) - 1;
+        sec = 60 - (time % 60);
+
+        // print remaining time
+        if(sec == 60 && min != 0)
+            System.out.println(name + "'s time left is " + (min + 1) + ":00");
+        else
+            System.out.println(name + "'s Time left is " + min + ":" + sec);
+    }
+
 }
